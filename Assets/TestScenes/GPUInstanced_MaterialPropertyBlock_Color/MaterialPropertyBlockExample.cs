dfc31a611066a51dee529df4ca1487a20d264472
MingWai Chan <mingwai@unity3d.com>
2023-03-15 15:30:05 +0100

    add MPB

---
+++ Assets/TestScenes/GPUInstanced_MaterialPropertyBlock_Color/MaterialPropertyBlockExample.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MaterialPropertyBlockExample : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    void Start()
+    {
+        MaterialPropertyBlock props = new MaterialPropertyBlock();
+        MeshRenderer renderer;
+
+        foreach (GameObject obj in objects)
+        {
+            float r = Random.Range(0.0f, 1.0f);
+            float g = Random.Range(0.0f, 1.0f);
+            float b = Random.Range(0.0f, 1.0f);
+            props.SetColor("_Color", new Color(r, g, b));
+
+            renderer = obj.GetComponent<MeshRenderer>();
+            renderer.SetPropertyBlock(props);
+        }
+    }
+}
